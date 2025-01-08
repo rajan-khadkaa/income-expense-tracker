56e307e19670f6e64b71a7cd2dc96fce27841e4a
rajan khadkaa <rajankhadkaa0809@gmail.com>
2025-01-08 13:51:39 +0545

    modified index.js for frontend

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const dashboardRoutes = require("./routes/dashboard.routes.js");
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: "https://income-expense-haql.onrender.com",
     credentials: true,
   })
 );
